b57042a7e0b9ed41933b4b30556d0198c1cfb12b
souss <soussiyacine03@gmail.com>
2026-08-01 19:48:55 +0100

    implementing hive,audio service and just_audio

---
+++ lib/data/audio_source_handler.dart
@@ -0,0 +1,66 @@
+import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
+
+import '../features/player/models/track.dart';
+
+class AudioSourceHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
+
+  final AudioPlayer _player = AudioPlayer();
+  void _broadcastState(PlaybackEvent event) {
+    playbackState.add(playbackState.value.copyWith(
+      playing: _player.playing,
+      updatePosition: _player.position,
+      controls: [
+        MediaControl.skipToPrevious,
+        _player.playing ? MediaControl.pause : MediaControl.play,
+        MediaControl.skipToNext,
+      ],
+    ));
+  }
+  Future<void> playTrack(Track track) async {
+    final item = _toMediaItem(track);
+    mediaItem.add(item);
+    await _player.setAudioSource(_toAudioSource(track, item));
+    await _player.play();
+  }
+
+  AudioSource _toAudioSource(Track track, MediaItem tag) {
+    return track.sourceType == AudioSourceType.remote
+        ? AudioSource.uri(Uri.parse(track.uri), tag: tag)
+        : AudioSource.uri(Uri.file(track.uri), tag: tag);
+  }
+
+  MediaItem _toMediaItem(Track track) => MediaItem(
+    id: track.id,
+    title: track.title,
+    artist: track.artist,
+    duration: track.duration,
+  );
+  @override
+  Future<void> play() => _player.play();
+
+  @override
+  Future<void> pause() => _player.pause();
+
+  @override
+  Future<void> seek(Duration position) => _player.seek(position);
+
+  @override
+  Future<void> skipToNext() => _player.seekToNext();
+
+  @override
+  Future<void> skipToPrevious() => _player.seekToPrevious();
+
+  @override
+  Future<void> stop() async {
+    await _player.stop();
+    await super.stop();
+  }
+  AudioSourceHandler() {
+    _player.playbackEventStream.listen(_broadcastState);
+  }
+
+///just_audio doesn't take a Track it takes an AudioSource, built from a Uri
+///and separately,audio_service wants a MediaItem like title/artist..etc to actually display in the notification
+///so loading one track means building both from our one Track
+}
---
+++ lib/data/local/hiveboxes.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+import 'package:hive_flutter/hive_flutter.dart';
+
+class Hiveboxes {
+  Hiveboxes._();
+  static const String tracks ='track_box';
+  static const String playlists = 'playlist_box';
+  static late Box<Map> tracksBox;
+  static late Box<Map> playlistsBox;
+///late is a kind of promise to the compiler that the given value is empty but it will be set
+  ///before anything tries to read it
+  ///we do this because hiveBox is async and we don't have the box yet so we wait until it finishes
+  static bool _initialized= false;
+  static Future<void> init () async {
+
+    if(_initialized) return;
+    await Hive.initFlutter();
+
+    tracksBox = await Hive.openBox<Map>(tracks);
+    playlistsBox = await Hive.openBox<Map>(playlists);
+    _initialized = true;
+  }
+}
+/// we are using Hive which is a lightweight database for Flutter that doesn’t require any complex setup or SQL queries
+/// It’s a NoSQL solution that allows you to store and retrieve data in a simple way
+/// more details on README.md
